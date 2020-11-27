7478d77c97c979588e171183d8a37c47aa2f003f
Digitlimit Inc <frankemeks77@yahoo.com>
2020-11-27 23:42:10 +0100

    feat: add rooms table migration

---
+++ database/migrations/2020_11_27_223941_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('hotel_id');//Hotel ID : [room belongsTo hotel]
+            $table->integer('room_type_id');//Room Type : ID of room type [room hasOne room_type]
+            $table->string('name');//Room Name (e.g. A1, B2, C4)
+            $table->integer('room_capacity_id');//Room Capacity : ID of room capacity [room hasOne room_capacity]
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
