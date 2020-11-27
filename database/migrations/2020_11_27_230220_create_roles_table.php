3e583627fb74a16209f2a95ba119e2ffeeffa2b0
Digitlimit Inc <frankemeks77@yahoo.com>
2020-11-28 00:03:54 +0100

    feat: add roles table migration

--- database/migrations/2020_11_27_230220_create_roles_table.php
+++ database/migrations/2020_11_27_230220_create_roles_table.php
@@ -14,7 +14,10 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
