24c91b5fc5be13e2c58fd781b0fd960ec4ec31fb
Digitlimit Inc <frankemeks77@yahoo.com>
2020-11-28 02:29:33 +0100

    feat: add prices and roles  seeder
    fix: #4

---
+++ config/global.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global config
+    |--------------------------------------------------------------------------
+    |
+    | One place to keep site configurations
+    |
+    */
+
+    'paths' => [
+        'image' => [
+            'hotel' => 'hotels',
+            'room'  => 'rooms'
+        ]
+    ],
+
+    'default_currency' => 'USD',
+    'default_hotel_id' => 1
+];
---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->delete();
+
+        DB::table('roles')->insert([
+            [
+                'name'         => 'administrator',
+                'display_name' => 'Administrator',
+                'description'  => 'Hotel administrator'
+            ],
+            [
+                'name'         => 'customer',
+                'display_name' => 'Customer',
+                'description'  => 'Hotel guests'
+            ],
+        ]);
+    }
+}
