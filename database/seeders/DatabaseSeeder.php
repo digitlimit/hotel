cc11a29b15b00a732679167d37002404cf5d17e0
Digitlimit Inc <frankemeks77@yahoo.com>
2020-11-28 02:21:05 +0100

    feat: add hotels  seeder
    fix: #4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
+        $this->call(HotelsTableSeeder::class);
         $this->call(BookingsTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
