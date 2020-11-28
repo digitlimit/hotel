824ce07d03462c5529702d12cf3fee026e8793cc
Digitlimit Inc <frankemeks77@yahoo.com>
2020-11-28 11:01:23 +0100

    feat: add room model
    fix: #5 - Create models

---
+++ app/Models/Room.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Room extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'rooms';
+
+    /**
+     * The database primary key value.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',//Room Name (e.g. A1, B2, C4)
+        'hotel_id',//Hotel  ID : ID of hotel [room belongsTo hotel]
+        'room_type_id',//Room Type : ID of room type [room hasOne room_type]
+        //'room_capacity_id',//Room Capacity : ID of room capacity [room hasOne room_capacity]
+        'image',//Room Image
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+}
